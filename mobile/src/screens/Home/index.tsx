79a8ca8910e9bb1a8907318528b2563d06b88555
Matheus Aguiar <first_mf@hotmail.com>
2021-10-24 16:28:12 -0300

    fix

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, KeyboardAvoidingView, Platform } from 'react-native';
 
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
@@ -14,11 +14,16 @@ export function Home() {
   const { user } = useAuth();
 
   return (
-    <View style={styles.container}>
-      <Header />
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <View style={styles.container}>
+        <Header />
 
-      <MessageList />
-      {user?.id ? <SendMessageForm /> : <SignInBox />}
-    </View>
+        <MessageList />
+        {user?.id ? <SendMessageForm /> : <SignInBox />}
+      </View>
+    </KeyboardAvoidingView>
   );
 }
